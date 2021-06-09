04bd5dc77e1a050f13651d631f7da1f685c32c0f
rasani8064 <81849484+rasani8064@users.noreply.github.com>
2021-06-09 08:04:54 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+/*
+   JavaScript Array Methods are
+ */
+
+// What is array ??
+/*
+The JavaScript Array class is a global object that is used in the construction of arrays; which are high-level, list-like objects.
+*/
+
+// create an array
+
+let fruits = ["apple", "mango"];
+
+console.log(fruits);
